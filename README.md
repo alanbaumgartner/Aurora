eee2d79802dcf2a9a8e6cff77b22ebcc10ae5c0f
Alan Baumgartner <alanvbaumgartner@gmail.com>
2018-03-19 02:32:42 +0900

    Improved Readability and started working on a config.

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ Aurora RAT or Remote Administration Tool is written in Go, using wxGo as a GUI t
 This project was started to learn networking in go and get myself started in the security field.
 
 ## External Libraries Required
+
 [wxGo](https://github.com/dontpanic92/wxGo)
--- Server.go
+++ Server.go
@@ -7,26 +7,30 @@ import (
 	"net"
 	"runtime"
 	"bufio"
+	"os/user"
+	"os"
+	"encoding/json"
+	"path/filepath"
+	"io/ioutil"
 )
 
-var a Aurora
-var app *AuroraFrame
+var server Aurora
+var gui *AuroraFrame
 
 type Aurora struct {
 	// ln is shortened "listener"
 	ln net.Listener
 
 	// cl is shortened "currently listening"
-	cl bool
+	curListen bool
+
+	// Aurora Settings Directory
+	settingsDir string
+	settingsFile string
+
+	// Aurora Settings
+	config Config
 }
-
-func newAurora() Aurora {
-	self := Aurora{}
-	self.cl = false
-
-	return a
-}
-
 
 type AuroraFrame struct {
 	wx.Frame
@@ -41,16 +45,35 @@ type AuroraFrame struct {
 	liveConns int
 }
 
+type Config struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+}
+
 func main() {
 
-	a = newAurora()
+	server = newAurora()
+	gui = newFrame()
 
-	app = newFrame()
+	server.loadSettings()
 
 	wx1 := wx.NewApp()
-	app.Show()
+	gui.Show()
 	wx1.MainLoop()
-	app.Destroy()
+	gui.Destroy()
+}
+
+func newAurora() Aurora {
+	self := Aurora{}
+	self.curListen = false
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+	}
+	self.settingsDir, _ = filepath.Abs(usr.HomeDir + "\\Aurora")
+	self.settingsFile, _ = filepath.Abs(self.settingsDir + "\\settings.json")
+
+	return self
 }
 
 // Aurora GUI Functions
@@ -63,71 +86,126 @@ func newFrame() *AuroraFrame {
 	self.Frame = wx.NewFrame(wx.NullWindow, -1, "Aurora RAT", wx.DefaultPosition, wx.NewSizeT(800, 600))
 	bSizer := wx.NewBoxSizer(wx.VERTICAL)
 
-	self.menubar = wx.NewMenuBar()
 
+	// Status bar
 	self.statusbar = self.CreateStatusBar()
 	self.statusbar.SetStatusText("Not Currently Listening.")
-
-	self.statusbar.SetCanFocus(false)
 
 	self.statusbar.SetFieldsCount(2)
 	self.statusbar.SetStatusText("Live Connections: " + strconv.Itoa(self.liveConns), 1)
 
 
+	// Menu bar
+	self.menubar = wx.NewMenuBar()
+
+	// Menu bar menus
 	menuFile := wx.NewMenu()
+
+	menuSettings := wx.NewMenu()
+
+	// File menu
 	self.menuItemListen = wx.NewMenuItem(menuFile, wx.ID_ANY, "&Start Listening", "Start Listening", wx.ITEM_NORMAL)
 	menuFile.Append(self.menuItemListen)
+	menuItemExit := wx.NewMenuItem(menuFile, wx.ID_ANY, "&Exit", "Exit", wx.ITEM_NORMAL)
+	menuFile.Append(menuItemExit)
 
+	// Settings menu
+	menuItemSave := wx.NewMenuItem(menuSettings, wx.ID_ANY, "&Save Settings", "Save Settings", wx.ITEM_NORMAL)
+	menuSettings.Append(menuItemSave)
+
+
+	// Finish up menus
 	self.menubar.Append(menuFile, "&File")
+	self.menubar.Append(menuSettings, "&Settings")
+
 	self.SetMenuBar(self.menubar)
 
-	wx.Bind(self, wx.EVT_MENU, eventSetListening, self.menuItemListen.GetId())
+	wx.Bind(self, wx.EVT_MENU, gui.eventSetListening, self.menuItemListen.GetId())
+	wx.Bind(self, wx.EVT_MENU, server.saveSettings, menuItemSave.GetId())
+
 	self.SetSizer(bSizer)
 	self.Layout()
 
 	return self
 }
 
-func eventSetListening(e wx.Event) {
+func (gui *AuroraFrame) eventSetListening(_ wx.Event) {
 
 	liveConns := make(chan int)
 
-	go setListening(liveConns)
-	go updateLiveConnections(liveConns)
+	go server.setListening(liveConns)
+	go gui.updateLiveConnections(liveConns)
 }
 
-func updateLiveConnections(liveConns chan int) {
+func (gui *AuroraFrame) updateLiveConnections(liveConns chan int) {
 	for i := range liveConns {
-		app.liveConns += i
-		app.statusbar.SetStatusText("Live Connections: " + strconv.Itoa(app.liveConns), 1)
+		gui.liveConns += i
+		gui.statusbar.SetStatusText("Live Connections: " + strconv.Itoa(gui.liveConns), 1)
 	}
 
 }
 
 // Aurora Backend Functions
 
-func setListening(liveConns chan int) {
+func (a *Aurora) loadSettings() {
+	configFile, err := ioutil.ReadFile(a.settingsFile)
+	if os.IsNotExist(err) {
+		_, err := os.Stat(a.settingsDir)
+		if os.IsNotExist(err) {
+			os.MkdirAll(a.settingsDir, os.ModeDir)
+		}
+		os.Create(a.settingsFile)
+		jsonData, err := json.Marshal(Config{
+			Host: "127.0.0.1",
+			Port: ":4371",
+			})
+		if err != nil {
+			fmt.Println(err)
+		}
+		ioutil.WriteFile(a.settingsFile, jsonData, 0777)
+		json.Unmarshal(configFile, &a.config)
+	} else if err != nil {
+		fmt.Println(err)
+	} else {
+		json.Unmarshal(configFile, &a.config)
+	}
+}
+
+func (server *Aurora) saveSettings(_ wx.Event) {
+	jsonData, err := json.Marshal(Config{
+		Host: "Test",
+		Port: ":123",
+		})
+	if err != nil {
+		fmt.Println(err)
+	}
+	ioutil.WriteFile(server.settingsFile, jsonData, 0777)
+}
+
+func (server *Aurora) setListening(liveConns chan int) {
 
 	ln := make(chan bool)
 
-	go a.SetListening(ln, liveConns);
+	go server.SetListening(ln, liveConns)
 
 	listening := <- ln
 
 	if listening == true {
-		app.statusbar.SetStatusText("Currently Listening.")
-		app.menuItemListen.SetItemLabel("Stop Listening")
+		gui.statusbar.SetStatusText("Currently Listening.")
+		gui.menuItemListen.SetHelp("Stop Listening")
+		gui.menuItemListen.SetItemLabel("Stop Listening")
 		wx.MessageBox("Started Listening.")
 	} else {
-		app.statusbar.SetStatusText("Not Currently Listening.")
-		app.menuItemListen.SetItemLabel("Start Listening")
+		gui.statusbar.SetStatusText("Not Currently Listening.")
+		gui.menuItemListen.SetHelp("Start Listening")
+		gui.menuItemListen.SetItemLabel("Start Listening")
 		wx.MessageBox("Stopped Listening.")
 	}
 }
 
 func (a *Aurora) SetListening(cln chan bool, liveConns chan int) bool {
-	a.cl = !a.cl
-	switch a.cl {
+	a.curListen = !a.curListen
+	switch a.curListen {
 	case false:
 		if err := a.ln.Close(); err != nil {
 			fmt.Println(err)
@@ -138,24 +216,24 @@ func (a *Aurora) SetListening(cln chan bool, liveConns chan int) bool {
 		break
 	}
 
-	cln <- a.cl
+	cln <- a.curListen
 	close(cln)
 
-	return a.cl
+	return a.curListen
 }
 
 func (a *Aurora) startListening(liveConns chan int) {
 
 	var err error
 
-	a.ln, err = net.Listen("tcp", ":25565")
+	a.ln, err = net.Listen("tcp", a.config.Port)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	for {
-		if !a.cl {
+		if !a.curListen {
 			runtime.Goexit()
 		}
 
@@ -175,7 +253,7 @@ func (a *Aurora) receive(conn net.Conn, liveConns chan int) {
 	connected := true
 
 	for {
-		if !a.cl {
+		if !a.curListen {
 			if connected {
 				connected = false
 			}
